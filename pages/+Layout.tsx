c7d110464d7fdf6cc3dc98376ff6994ea46f3688
cyrill62 <cyril@lepagnot.fr>
2026-06-28 11:33:25 +0200

    Removes Sidebar & add Menu

--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -6,12 +6,7 @@ import { Link } from "../components/Link";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className={"flex max-w-5xl m-auto"}>
-      <Sidebar>
-        <Logo />
-        <Link href="/">Welcome</Link>
-        <Link href="/todo">Todo</Link>
-        <Link href="/star-wars">Data Fetching</Link>
-      </Sidebar>
+      <Menu />
       <Content>{children}</Content>
     </div>
   );
@@ -25,22 +20,50 @@ function Sidebar({ children }: { children: React.ReactNode }) {
   );
 }
 
+const Menu = () => (
+  {/* popoverTarget must match the ID of the corresponding popover, and each ID must be unique */}
+<button className="btn sm:hidden" popoverTarget="my-megamenu-1">Menu</button>
+
+<div className="megamenu max-sm:megamenu-vertical p-2 border border-base-300" id="my-megamenu-1" popover>
+  <span className="megamenu-active"></span>
+
+  <button popoverTarget="a1">Services</button>
+  <div id="a1" popover>
+    <ul className="menu">
+      <li><a>Enterprise</a></li>
+      <li><a>CRM software</a></li>
+      <li><a>Security</a></li>
+      <li><a>Consulting</a></li>
+    </ul>
+  </div>
+
+  <button popoverTarget="a2">AI</button>
+  <div id="a2" popover>
+    <ul className="menu">
+      <li><a>AI infrastructure</a></li>
+      <li><a>Image generation</a></li>
+      <li><a>MCP servers</a></li>
+    </ul>
+  </div>
+
+  <button popoverTarget="a3">Cloud Solutions</button>
+  <div id="a3" popover>
+    <ul className="menu">
+      <li><a>Cloud computing</a></li>
+      <li><a>Storage solutions</a></li>
+      <li><a>Database services</a></li>
+      <li><a>CDN performance</a></li>
+    </ul>
+  </div>
+</div>
+)
+
 function Content({ children }: { children: React.ReactNode }) {
   return (
     <div id="page-container">
       <div id="page-content" className={"p-5 pb-12 min-h-screen"}>
         {children}
       </div>
-    </div>
-  );
-}
-
-function Logo() {
-  return (
-    <div className={"p-5 mb-2"}>
-      <a href="/">
-        <img src={logoUrl} height={64} width={64} alt="logo" />
-      </a>
     </div>
   );
 }
