289fa07689bd8aef3a8d575e46e05e10364ef1d4
cyrill62 <cyril@lepagnot.fr>
2026-06-30 19:28:53 +0200

    Update daisyUI to 5.6.6 (for megamenu)

--- pages/+Layout.tsx
+++ pages/+Layout.tsx
@@ -1,6 +1,5 @@
 import "./Layout.css";
 import "./tailwind.css";
-import logoUrl from "../assets/logo.svg";
 import { Link } from "../components/Link";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
