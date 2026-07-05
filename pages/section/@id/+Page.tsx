a2d738883b35119279865e6e3b341a89a5824063
cyrill62 <cyril@lepagnot.fr>
2026-07-05 09:52:35 +0200

    Turn items into a carousel

--- pages/section/@id/+Page.tsx
+++ pages/section/@id/+Page.tsx
@@ -1,8 +1,8 @@
 import { useQuery } from "@apollo/client/react";
 import { gql } from "@apollo/client";
 import ReactMarkdown from "react-markdown";
-const badgeColors = [/*'primary', 'accent', */'info', 'warning', 'error', 'success', 'secondary']
-const badgeVariants = ['soft', 'outline', 'dash', '']
+const badgeColors = [/*'primary', 'accent', */ "info", "warning", "error", "success", "secondary"];
+const badgeVariants = ["soft", "outline", "dash", ""];
 const Page = () => {
   const { data }: { data: any } = useQuery(gql`
     {
@@ -55,19 +55,33 @@ const Page = () => {
             </li>
           ))}
         </ul>
-        <ul>
-          {section.items.map((item) => (
-            <li>
-              <iframe src={item.url} alt={item.url} width="300" height="300" />
-              <div>
+        <div className="carousel w-full">
+          {section.items.map((item, i) => (
+            <div id={`item-${i}`}  className="carousel-item relative w-full">
+              <iframe src={item.url} alt={item.url} width="300" height="300" className="w-full" />
+              <div className="caption">
                 <ReactMarkdown>{item.desc}</ReactMarkdown>
               </div>
               <div>
-                {item.tags.map((tag, i) =>(<div className={`badge badge-${badgeVariants[i % badgeVariants.length]} badge-${badgeColors[i % badgeColors.length]} me-2`}>#{tag}</div>))}
+                {item.tags.map((tag, i) => (
+                  <div
+                    className={`badge badge-${badgeVariants[i % badgeVariants.length]} badge-${badgeColors[i % badgeColors.length]} me-2`}
+                  >
+                    #{tag}
+                  </div>
+                ))}
               </div>
-            </li>
+              <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                <a href={`#item-${i - 1}`} className="btn btn-circle">
+                  ❮
+                </a>
+                <a href={`#item-${i + 1}`} className="btn btn-circle">
+                  ❯
+                </a>
+              </div>
+            </div>
           ))}
-        </ul>
+        </div>
       </>
     );
   } else {
